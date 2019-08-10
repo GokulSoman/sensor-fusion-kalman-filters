3dd39796266a3df354d06a41a4df5fca7095bef8
Neelesh Rastogi <rastogineelesh1@gmail.com>
2019-08-09 22:17:24 -0400

    Initialize repository with CMake

---
+++ cmake-build-debug/CMakeFiles/sensor_fusion_kalman_filters.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sensor_fusion_kalman_filters.dir/main.cpp.o"
+  "sensor_fusion_kalman_filters.pdb"
+  "sensor_fusion_kalman_filters"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sensor_fusion_kalman_filters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
